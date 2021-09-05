5ed7e1f3bbd425d8437e3e3c0abfc52b9b57555c
PaperbagLife <43522930+PaperbagLife@users.noreply.github.com>
2021-09-05 18:15:06 -0400

    uh

---
+++ Assets/Scripts/turret.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turret : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isBlue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for enemy in range, depending on self tag.
+        if(isBlue) {
+            // check for red 
+        }
+    }
+}
